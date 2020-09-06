a9508b838af12ef4aa0554f6ea63a1d518ba6db6
Jérémy Chauvet <jer.chauvet@gmail.com>
2020-09-06 17:35:39 +0200

    Adding missing environment variables.

--- lambda/functions/twitter-get-user/handler.py
+++ lambda/functions/twitter-get-user/handler.py
@@ -1,7 +1,7 @@
 import json
 import tweepy
+from tweepy.auth import OAuthHandler
 import os
-
 
 def main(event, context):
 
@@ -23,16 +23,16 @@ def main(event, context):
 
 def getUser():
   # assign the values accordingly
-  consumer_key = os.environ['TWITTER_CONSUMER_KEY']
-  consumer_secret = os.environ['TWITTER_CONSUMER_SECRET_KEY']
-  #access_token = ""
-  #access_token_secret = ""
+  consumer_key        = os.environ['TWITTER_CONSUMER_KEY']
+  consumer_secret     = os.environ['TWITTER_CONSUMER_SECRET']
+  access_token        = os.environ['TWITTER_ACCESS_TOKEN']
+  access_token_secret = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
 
   # authorization of consumer key and consumer secret
   auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 
   # set access to user's access key and access secret
-  #auth.set_access_token(access_token, access_token_secret)
+  auth.set_access_token(access_token, access_token_secret)
 
   # calling the api
   api = tweepy.API(auth)
