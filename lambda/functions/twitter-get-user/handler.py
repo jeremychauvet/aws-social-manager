75e2cff4106a30582cd8b155cc566a09d94cbe34
Jérémy Chauvet <jer.chauvet@gmail.com>
2020-09-10 22:31:16 +0200

    Improve readability with linter.

--- lambda/functions/twitter-get-user/handler.py
+++ lambda/functions/twitter-get-user/handler.py
@@ -2,38 +2,33 @@ import json
 import tweepy
 import os
 
+
 def main(event, context):
-  
-  body = {
-      "message": "Go Serverless v1.0! Your function executed successfully!"
-  }
 
-  response = {
-      "statusCode": 200,
-      "body": json.dumps(body)
-  }
+    body = {"message": "Your function executed successfully!"}
 
-  return response
+    return {"statusCode": 200, "body": json.dumps(body)}
+
 
 def getUser():
-  # assign the values accordingly
-  consumer_key        = os.environ['TWITTER_CONSUMER_KEY']
-  consumer_secret     = os.environ['TWITTER_CONSUMER_SECRET']
-  access_token        = os.environ['TWITTER_ACCESS_TOKEN']
-  access_token_secret = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
+    # assign the values accordingly
+    consumer_key = os.environ["TWITTER_CONSUMER_KEY"]
+    consumer_secret = os.environ["TWITTER_CONSUMER_SECRET"]
+    access_token = os.environ["TWITTER_ACCESS_TOKEN"]
+    access_token_secret = os.environ["TWITTER_ACCESS_TOKEN_SECRET"]
 
-  # authorization of consumer key and consumer secret
-  auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    # authorization of consumer key and consumer secret
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 
-  # set access to user's access key and access secret
-  auth.set_access_token(access_token, access_token_secret)
+    # set access to user's access key and access secret
+    auth.set_access_token(access_token, access_token_secret)
 
-  # calling the api
-  api = tweepy.API(auth)
+    # calling the api
+    api = tweepy.API(auth)
 
-  # fetching the statuses
-  statuses = api.home_timeline()
+    # fetching the statuses
+    statuses = api.home_timeline()
 
-  # printing the screen names of each status
-  for status in statuses:
-   print(status.user.screen_name)
+    # printing the screen names of each status
+    for status in statuses:
+        print(status.user.screen_name)
