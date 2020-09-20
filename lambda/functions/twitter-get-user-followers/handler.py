3b4b2282991924e349e9d00f27eba68c7f001934
Jérémy Chauvet <jer.chauvet@gmail.com>
2020-09-20 18:20:26 +0200

    Change folder architecture to support multi function

---
+++ lambda/functions/twitter-get-user-followers/handler.py
@@ -0,0 +1,29 @@
+import tweepy
+import os
+import time
+
+def main(event, context):
+
+    return {"statusCode": 200, "output": getUserFollowers(event['userId'])}
+
+
+def getUserFollowers(userId):
+    consumer_key = os.environ["TWITTER_CONSUMER_KEY"]
+    consumer_secret = os.environ["TWITTER_CONSUMER_SECRET"]
+    access_token = os.environ["TWITTER_ACCESS_TOKEN"]
+    access_token_secret = os.environ["TWITTER_ACCESS_TOKEN_SECRET"]
+
+    # Login to Twitter API.
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(
+        auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, compression=True
+    )
+
+    # Fetching user followers.
+    followers = []
+    for page in tweepy.Cursor(api.followers_ids, user_id=userId).pages():
+        followers.extend(page)
+        time.sleep(2)
+
+    return len(followers)
