8926c20a72031519318b39ecf2d169db71528d85
Jérémy Chauvet <jer.chauvet@gmail.com>
2020-09-22 19:24:28 +0200

    Adding DynamoDB table.

---
+++ infrastructure/dynamodb.tf
@@ -0,0 +1,18 @@
+resource "aws_dynamodb_table" "social-manager-database" {
+  name           = "social-manager"
+   billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+    write_capacity = 1
+  hash_key       = "UserId"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  tags = {
+    Component   = "social-manager"
+    Environment = "dev"
+    CreatedBy   = "Jeremy Chauvet"
+  }
+}
