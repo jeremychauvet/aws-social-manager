f8da8590b532ee6ab5038d39e28ed67be139aa04
Jérémy Chauvet <jer.chauvet@gmail.com>
2020-09-30 22:26:44 +0200

    Linter fix.

--- infrastructure/dynamodb.tf
+++ infrastructure/dynamodb.tf
@@ -1,8 +1,8 @@
 resource "aws_dynamodb_table" "social-manager-database" {
   name           = "social-manager"
-   billing_mode   = "PROVISIONED"
+  billing_mode   = "PROVISIONED"
   read_capacity  = 1
-    write_capacity = 1
+  write_capacity = 1
   hash_key       = "UserId"
 
   attribute {
