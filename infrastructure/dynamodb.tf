dcdb7a2798b27b39bf123b44f1e531bd8ebe775e
Jérémy Chauvet <jer.chauvet@gmail.com>
2020-10-04 12:46:31 +0200

    Create DynamoDB table to store users to follow.

--- infrastructure/dynamodb.tf
+++ infrastructure/dynamodb.tf
@@ -1,5 +1,5 @@
-resource "aws_dynamodb_table" "social-manager-database" {
-  name           = "social-manager"
+resource "aws_dynamodb_table" "social-manager-db-user-followed" {
+  name           = "social-manager-user-followed"
   billing_mode   = "PROVISIONED"
   read_capacity  = 1
   write_capacity = 1
@@ -11,7 +11,26 @@ resource "aws_dynamodb_table" "social-manager-database" {
   }
 
   tags = {
-    Component   = "social-manager"
+    App   = "aws-social-manager"
+    Environment = "dev"
+    CreatedBy   = "Jeremy Chauvet"
+  }
+}
+
+resource "aws_dynamodb_table" "social-manager-db-user-to-follow" {
+  name           = "social-manager-user-to-follow"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "UserId"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  tags = {
+    App   = "aws-social-manager"
     Environment = "dev"
     CreatedBy   = "Jeremy Chauvet"
   }
