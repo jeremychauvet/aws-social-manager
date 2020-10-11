c2048319d79a472950db636962d77e08b2c1977e
Jérémy Chauvet <jer.chauvet@gmail.com>
2020-10-11 16:25:13 +0200

    Allowing to access S3 without Cloudfront.

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -1,11 +1,11 @@
-# Policy definition.
 data "aws_iam_policy_document" "public-read-get-object" {
+  # Allow Cloudfront to serve content.
   statement {
     principals {
       type        = "AWS"
       identifiers = [aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn]
     }
-    sid     = "PublicReadGetObject"
+    sid     = "PublicReadGetObjectFromCloudfront"
     actions = ["s3:GetObject"]
     resources = [
       "arn:aws:s3:::${var.dns_domain}/*",
@@ -15,5 +15,22 @@ data "aws_iam_policy_document" "public-read-get-object" {
     #   variable = "aws:SourceIp"
     #   values   = var.front_allowed_ips
     # }
+  }
+
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    sid     = "PublicReadGetObjectDirectAccess"
+    actions = ["s3:GetObject"]
+    resources = [
+      "arn:aws:s3:::${var.dns_domain}/*",
+    ]
+    condition {
+      test     = "IpAddress"
+      variable = "aws:SourceIp"
+      values   = var.front_allowed_ips
+    }
   }
 }
