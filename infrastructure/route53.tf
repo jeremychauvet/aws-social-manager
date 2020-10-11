23c00b5b930fe724408de61542ccbe20afb02d36
Jérémy Chauvet <jer.chauvet@gmail.com>
2020-10-11 12:13:53 +0200

    Redirect domain traffic to Cloudfront.

--- infrastructure/route53.tf
+++ infrastructure/route53.tf
@@ -6,17 +6,11 @@ data "aws_route53_zone" "main" {
 resource "aws_route53_record" "root_domain" {
   zone_id = data.aws_route53_zone.main.zone_id
   name    = var.dns_domain
-  type    = "A"
-
-  # alias {
-  #   name = "${aws_cloudfront_distribution.cdn.domain_name}"
-  #   zone_id = "${aws_cloudfront_distribution.cdn.hosted_zone_id}"
-  #   evaluate_target_health = false
-  # }
+  type    = "CNAME"
 
   alias {
-    zone_id                = aws_s3_bucket.frontend.hosted_zone_id
-    name                   = aws_s3_bucket.frontend.website_domain
+    name                   = aws_cloudfront_distribution.cdn.domain_name
+    zone_id                = aws_cloudfront_distribution.cdn.hosted_zone_id
     evaluate_target_health = false
   }
 }
