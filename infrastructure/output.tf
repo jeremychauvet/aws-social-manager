1f20411f2960b2fc4bab730c58bcf3b797123aaf
Jérémy Chauvet <jer.chauvet@gmail.com>
2020-10-11 16:03:11 +0200

    Adding CDN domain to output.

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -2,6 +2,6 @@ output "route53_domain" {
   value = aws_route53_record.root_domain.fqdn
 }
 
-# output "cdn_domain" {
-#   value = "${aws_cloudfront_distribution.cdn.domain_name}"
-# }
+output "cdn_domain" {
+  value = aws_cloudfront_distribution.cdn.domain_name
+}
