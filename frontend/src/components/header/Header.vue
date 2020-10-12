59b7a6914c051305752c7168b5dfd05cbce0ead5
Jérémy Chauvet <jer.chauvet@gmail.com>
2020-10-12 20:59:11 +0200

    Improve header responsive.

--- frontend/src/components/header/Header.vue
+++ frontend/src/components/header/Header.vue
@@ -5,18 +5,11 @@
             <b-navbar-item>
                 <router-link to="/">Home</router-link>
             </b-navbar-item>
-            <b-navbar-item>
-                <router-link to="/about">About</router-link>
-            </b-navbar-item>
         </template>
 
         <template slot="end">
             <b-navbar-item tag="div">
-                <div class="buttons">
-                    <button class="button is-light">
-                        <router-link to="/login">Login</router-link>
-                    </button>
-                </div>
+                <router-link to="/login">Login</router-link>
             </b-navbar-item>
         </template>
     </b-navbar>
