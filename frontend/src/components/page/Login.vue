ede33c346472eb81cf789acaa0c5b83c7b4b0522
Jérémy Chauvet <jer.chauvet@gmail.com>
2020-10-12 08:19:30 +0200

    Create login form.

---
+++ frontend/src/components/page/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <b-field label="Name">
+        <b-input placeholder="What is your name ?"></b-input>
+    </b-field>
+
+    <b-field label="Email">
+        <b-input type="email" placeholder="What is you email ?" maxlength="30"></b-input>
+    </b-field>
+
+    <b-field label="Password">
+        <b-input type="password" maxlength="30"></b-input>
+    </b-field>
+
+    <div class="buttons">
+      <b-button type="is-info is-light" expanded>Login</b-button>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
